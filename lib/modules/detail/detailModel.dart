3cf7bb3ea73d1e8ac9af46fe3576fd4a0ff9181b
Hades-li <121318100@qq.com>
2018-10-02 18:06:08 +0800

    更新

--- lib/modules/detail/detailModel.dart
+++ lib/modules/detail/detailModel.dart
@@ -10,12 +10,14 @@ class DetailItem {
     final String detail_source;// 媒体
     final int publish_time; // 发布时间
     final int comment_count; // 评论数
+    final String content; // html内容
     DetailItem({
         this.title,
         this.avatar_url,
         this.detail_source,
         this.publish_time,
-        this.comment_count
+        this.comment_count,
+        this.content
     });
 
     factory DetailItem.fromJson(Map<String, dynamic> json) => _$DetailItemFromJson(json);
--- lib/views/detail.dart
+++ lib/views/detail.dart
@@ -5,6 +5,9 @@ import 'dart:convert';
 import '../store/api.dart';
 import '../modules/detail/detailModel.dart';
 import '../store/index.dart';
+import 'package:flutter_image/network.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:flutter_html/flutter_html.dart';
 
 class Detail extends StatefulWidget {
     final String id;
@@ -48,6 +51,7 @@ class _DetailState extends State<Detail> {
         // TODO: implement initState
         print('初始化');
         super.initState();
+        timeago.setLocaleMessages('zh_CN', timeago.ZhCnMessages());
         _reqData(item_id: widget.id);
     }
 
@@ -67,27 +71,80 @@ class _DetailState extends State<Detail> {
                 iconTheme: Theme.of(context).copyWith(brightness: Brightness.light).iconTheme,
                 textTheme: Theme.of(context).copyWith(brightness: Brightness.light).textTheme,
                 elevation: 0.0,
-            ),
-            body: new SafeArea(
-                child: new Padding(
-                    padding: new EdgeInsets.all(15.0),
-                    child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                            new Text(
-                                detailItem?.title ?? gModel.num.toString(),
-                                softWrap: true,
-                                textAlign: TextAlign.left,
-                                textDirection: TextDirection.ltr,
-                                style: new TextStyle(fontSize: 24.0),
-                            ),
-                            new Row(
-                                mainAxisAlignment: MainAxisAlignment
-                                    .spaceBetween,
-                                children: <Widget>[],
-                            )
-                        ],
+                bottom: new PreferredSize(
+                    child: new Divider(
+                        height: 1.0,
+                        color: Colors.grey,
                     ),
+                    preferredSize: new Size(300.0, 1.0)
+                )
+            ),
+            body: new SingleChildScrollView(
+                child: new SafeArea(
+                    child: new Padding(
+                        padding: new EdgeInsets.all(15.0),
+                        child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                                new Text(
+                                    detailItem?.title ?? '',
+                                    softWrap: true,
+                                    textAlign: TextAlign.left,
+                                    textDirection: TextDirection.ltr,
+                                    style: new TextStyle(fontSize: 24.0),
+                                ),
+                                new Padding(
+                                    padding: const EdgeInsets.only(top: 10.0,bottom: 10.0),
+                                    child: new Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                            new Row(
+                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                crossAxisAlignment: CrossAxisAlignment.center,
+                                                children: <Widget>[
+                                                    new SizedBox(
+                                                        child: new CircleAvatar(
+                                                            backgroundImage: new NetworkImageWithRetry(detailItem?.avatar_url ?? ''),
+                                                            backgroundColor: Colors.white,
+                                                        ),
+                                                        width: 48.0,
+                                                        height: 48.0,
+                                                    ),
+                                                    new Padding(
+                                                        padding: const EdgeInsets.only(left: 10.0),
+                                                        child: new Column(
+                                                            mainAxisAlignment: MainAxisAlignment.start,
+                                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                                            children: <Widget>[
+                                                                new Text(
+                                                                    detailItem?.detail_source ?? '',
+                                                                    style: new TextStyle(
+                                                                        fontSize: 16.0,
+                                                                        fontWeight: FontWeight.bold,
+                                                                        color: new Color(0xff222222)
+                                                                    ),
+                                                                ),
+                                                                new Text(
+                                                                    detailItem != null ? timeago.format(new DateTime.fromMillisecondsSinceEpoch(detailItem.publish_time * 1000),locale: 'zh_CN') : '',
+                                                                    style: new TextStyle(
+                                                                        fontSize: 14.0,
+                                                                        color: new Color(0xff9d9d9d)
+                                                                    ),
+                                                                )
+                                                            ],
+                                                        ),
+                                                    )
+                                                ],
+                                            ),
+                                        ],
+                                    ),
+                                ),
+                                new Html(
+                                    data: detailItem?.content ?? '<p>掉毛没有</p>'
+                                )
+                            ],
+                        ),
+                    )
                 )
             )
         );
